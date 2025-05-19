69e405ae50c652e059d6349c2f0be54269a55121
Niels Provos <provos@gmail.com>
2025-05-19 09:57:09 -0700

    feat: add Tool to __all__ exports in __init__.py

--- llm_interface/__init__.py
+++ llm_interface/__init__.py
@@ -2,7 +2,7 @@ from ollama import ListResponse
 
 from .llm_config import llm_from_config
 from .llm_interface import LLMInterface, ModelError
-from .llm_tool import tool
+from .llm_tool import Tool, tool
 from .token_usage import TokenUsage
 
 __version__ = "0.1.11"
@@ -10,6 +10,7 @@ __all__ = [
     "LLMInterface",
     "llm_from_config",
     "tool",
+    "Tool",
     "ListResponse",
     "TokenUsage",
     "ModelError",
